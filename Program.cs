c9bb762d35d200a53104b9849474ffddda9bb5d8
Kevin Horn <khorn89@gmail.com>
2018-05-10 20:13:51 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+
+namespace WebDriverDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initilize Driver
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Url = "http://www.google.com";
+
+        }
+    }
+}
